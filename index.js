d15840388feb4e915968dda44be071c3b76426be
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-01-26 15:11:00 +0100

    done

--- index.js
+++ index.js
@@ -47,15 +47,29 @@
 
 // task 5
 
-const testInput = document.querySelector("#test");
-const resultInput = document.querySelector("#result");
-const button = document.querySelector("button");
+// const testInput = document.querySelector("#test");
+// const resultInput = document.querySelector("#result");
+// const button = document.querySelector("button");
 
-button.addEventListener("click", result);
-function result() {
-  if (isNaN(testInput.value)) {
-    alert("Ввели не число");
-  } else {
-    resultInput.value = testInput.value * testInput.value;
-  }
+// button.addEventListener("click", result);
+// function result() {
+//   if (isNaN(testInput.value)) {
+//     alert("Ввели не число");
+//   } else {
+//     resultInput.value = testInput.value * testInput.value;
+//   }
+// }
+
+// task 6
+
+const title = document.querySelector("h1");
+const list = document.createElement("ul");
+let message = prompt();
+while (message !== null) {
+  const listItem = document.createElement("li");
+  listItem.textContent = message;
+  list.appendChild(listItem);
+  message = prompt();
 }
+
+title.after(list);
